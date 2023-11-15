b9b89c21f4f9ff20789a2c33d6b8a02a2bd8cdb0
Raphael Ramos <raphael.opensource@gmail.com>
2023-11-15 09:40:04 -0300

    fix: database setup

--- src/web-server/config/db.php
+++ src/web-server/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=eimports;port=3360',
     'username' => 'root',
-    'password' => '',
+    'password' => 'root',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
--- src/web-server/config/web.php
+++ src/web-server/config/web.php
@@ -15,6 +15,9 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '_dHHsJHGmzcn5lYVFEEyCQ0uKJhn3CMN',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -42,14 +45,14 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
+            'enableStrictParsing' => true,
             'showScriptName' => false,
             'rules' => [
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'user'],
             ],
-        ],
-        */
+         ]
     ],
     'params' => $params,
 ];
