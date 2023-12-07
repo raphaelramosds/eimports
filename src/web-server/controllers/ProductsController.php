c733dafe87576c393c86ca0599110b17d7280a0f
Raphael Ramos <raphael.opensource@gmail.com>
2023-12-07 16:01:05 -0300

    feat: purchase orders migrations

--- src/web-server/controllers/ProductsController.php
+++ src/web-server/controllers/ProductsController.php
@@ -2,7 +2,19 @@
 
 namespace app\controllers;
 
+use app\components\SellerOwnedRecordsTrait;
+use app\models\Product;
+
 class ProductsController extends ActiveController
 {
+    use SellerOwnedRecordsTrait;
     public $modelClass = 'app\models\Product';
+
+    /**
+     * @throws ForbiddenHttpException
+     */
+    public function actionIndex()
+    {
+        return $this->getSellerOwnedRecords(new Product());
+    }
 }
---
+++ src/web-server/database/changelog/0.1/2023/1207-purchase-orders.sql
@@ -0,0 +1,44 @@
+--liquibase formatted sql
+
+--changeset franklin:20231207:01
+CREATE TABLE client (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    phone_num VARCHAR(20) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    seller_id INT NOT NULL,
+    CONSTRAINT `fk_client_seller`
+        FOREIGN KEY (seller_id) REFERENCES seller (id)
+        ON DELETE CASCADE
+);
+
+--changeset franklin:20231207:02
+CREATE TABLE purchase_order (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    purchase DATE NOT NULL,
+    settlement DATE,
+    receipt VARCHAR(255),
+    client_id INT NOT NULL,
+    seller_id INT NOT NULL,
+    CONSTRAINT `fk_purchase_order_client`
+        FOREIGN KEY (client_id) REFERENCES client (id)
+        ON DELETE CASCADE,
+    CONSTRAINT `fk_purchase_order_seller`
+        FOREIGN KEY (seller_id) REFERENCES seller (id)
+        ON DELETE CASCADE
+);
+
+
+--changeset franklin:20231207:03
+CREATE TABLE product_purchase_order (
+    purchase_order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    price DECIMAL(6,2) NOT NULL,
+    quantity INT NOT NULL,
+    PRIMARY KEY (purchase_order_id, product_id),
+    CONSTRAINT `fk_purchase_order`
+        FOREIGN KEY (purchase_order_id) REFERENCES purchase_order (id)
+        ON DELETE CASCADE,
+    CONSTRAINT `fk_product_order`
+        FOREIGN KEY (product_id) REFERENCES product (id)
+        ON DELETE CASCADE
+);
