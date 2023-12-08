0b26667f74fe628322f09379e20c79a464705030
franssoares <xflsn100@gmail.com>
2023-12-08 17:57:16 -0300

    fix: sql code

--- src/web-server/database/changelog/0.1/2023/1207-purchase-orders.sql
+++ src/web-server/database/changelog/0.1/2023/1207-purchase-orders.sql
@@ -1,5 +1,25 @@
 --liquibase formatted sql
 
+-- vendedor
+CREATE TABLE seller (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    login VARCHAR(25) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    name VARCHAR(50) NOT NULL
+);
+
+--category
+CREATE TABLE category (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(255),
+    seller_id INT NOT NULL,
+    CONSTRAINT fk_category_seller
+        FOREIGN KEY (seller_id) REFERENCES seller (id)
+        ON DELETE CASCADE
+);
+
+--cliente
 --changeset franklin:20231207:01
 CREATE TABLE client (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -11,6 +31,7 @@ CREATE TABLE client (
         ON DELETE CASCADE
 );
 
+--ordem
 --changeset franklin:20231207:02
 CREATE TABLE purchase_order (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -27,7 +48,24 @@ CREATE TABLE purchase_order (
         ON DELETE CASCADE
 );
 
+--produto
+CREATE TABLE product (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(255),
+    stock SMALLINT UNSIGNED,
+    quotation DECIMAL(6,2),
+    category_id INT,
+    seller_id INT NOT NULL,
+    CONSTRAINT fk_product_category
+        FOREIGN KEY (category_id) REFERENCES category (id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_product_seller
+        FOREIGN KEY (seller_id) REFERENCES seller (id)
+        ON DELETE CASCADE
+);
 
+--ordem_produto
 --changeset franklin:20231207:03
 CREATE TABLE product_purchase_order (
     purchase_order_id INT NOT NULL,
